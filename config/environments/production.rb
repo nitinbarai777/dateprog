5c7953466fa64974c47bdcd9e5ef5be883335134
nitin <nitin.barai777@gmail.com>
2017-01-10 11:47:42 +0530

    Mailer setting changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,12 +76,12 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
-    address:        'smtp.gmail.com',
-    port:           587,
-    domain:         'gmail.com',
-    user_name:      ENV["GMAIL_SMTP_USER"],
-    password:       ENV["GMAIL_SMTP_PASSWORD"],
-    authentication: 'plain',
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => ENV["GMAIL_SMTP_USER"],
+    :password =>  ENV["GMAIL_SMTP_PASSWORD"],
+    :authentication => 'plain',
     :enable_starttls_auto => true
   }
   
